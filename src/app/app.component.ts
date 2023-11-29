e0ef616f8271bc24eb6ff68e6a5fcf33733dd378
KYL <khangnguyen282002@gmail.com>
2023-11-29 17:31:37 +0700

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { AuthService } from './shared/auth.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  userData: any = JSON.parse(localStorage.getItem('user')!);
+  constructor(
+    private authService : AuthService
+  ) {}
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,37 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+// export const environment = {
+//   production: false,
+//   firebaseConfig: {
+//     apiKey: "AIzaSyCTnHLLslG6r4XB831sECr8U6BVI9KVvpE",
+//   authDomain: "medlife-5a06c.firebaseapp.com",
+//   projectId: "medlife-5a06c",
+//   storageBucket: "medlife-5a06c.appspot.com",
+//   messagingSenderId: "29658981015",
+//   appId: "1:29658981015:web:ad21832724fbd4ffeb7fd2",
+//   measurementId: "G-VKCRDVPZB9"
+//   }
+// };
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDliy4_AowrgAXjUFrNG2Wu7imGxO0c_po",
+    authDomain: "med-store-ionic-f5c3d.firebaseapp.com",
+    projectId: "med-store-ionic-f5c3d",
+    storageBucket: "med-store-ionic-f5c3d.appspot.com",
+    messagingSenderId: "958812155324",
+    appId: "1:958812155324:web:066a7148cd7834e90c8fb9",
+    measurementId: "G-BZWZX2F9KD"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
